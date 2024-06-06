6aa88ca9cad0065fbadcea127b96a1d5aeeb3b0d
Ruslan Podurets <ruslan.podurets.apps@gmail.com>
2024-06-06 10:28:21 +0300

    initialized repo

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id(Plugins.AndroidLibrary)
+    id(Plugins.JetBrainsKotlinAndroid)
+}
+
+android {
+    namespace = "lib.android.components"
+    compileSdk = Configs.compileSdk
+
+    defaultConfig {
+        minSdk = Configs.minSdk
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = Configs.Lang.sourceCompatibility()
+        targetCompatibility = Configs.Lang.targetCompatibility()
+    }
+    kotlinOptions {
+        jvmTarget = Configs.Lang.jvmTarget
+    }
+}
+
+dependencies {
+    implementation(Libs.AndroidXCoreKtx)
+    implementation(Libs.Appcompat)
+    implementation(Libs.Material)
+}
